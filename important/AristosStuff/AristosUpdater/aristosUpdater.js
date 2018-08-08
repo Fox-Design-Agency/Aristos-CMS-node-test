0824e0d8668c06305f9e89379de9945cf07a7f4c
Christopher Fox <cfox1290@gmail.com>
2018-08-07 22:00:29 -0600

    basic ui updates

--- important/AristosStuff/AristosUpdater/aristosUpdater.js
+++ important/AristosStuff/AristosUpdater/aristosUpdater.js
@@ -9,24 +9,24 @@ const fs = require("fs-extra");
 * GET Latest Update
 */
 router.get("/", isAdmin, (req, res, next) => {
-  request.get(
-    "https://b5tx3g61ie.execute-api.us-east-2.amazonaws.com/default/AristosBasicUpdater",
+  // request.get(
+  //   "https://b5tx3g61ie.execute-api.us-east-2.amazonaws.com/default/AristosBasicUpdater",
 
-    function(error, response, body) {
-      if (error) {
-        req.flash("error_msg", "There was an error with the Update!");
-        //log error
-        res.redirect("back");
-        return console.error("upload failed:", error);
-      }
+  //   function(error, response, body) {
+  //     if (error) {
+  //       req.flash("error_msg", "There was an error with the Update!");
+  //       //log error
+  //       res.redirect("back");
+  //       return console.error("upload failed:", error);
+  //     }
       
-      const content = JSON.parse(body);
-      content.forEach(stuff => {
-        fs.outputFile(stuff.name, stuff.content);
-      });
-    }
-  );
-  req.flash("success_msg", "System Updated!");
+  //     const content = JSON.parse(body);
+  //     content.forEach(stuff => {
+  //       fs.outputFile(stuff.name, stuff.content);
+  //     });
+  //   }
+  // );
+  // req.flash("success_msg", "System Updated!");
   res.redirect("back");
 });
 
